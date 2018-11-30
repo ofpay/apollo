707c7713e185e54f8c5fb6e55b605c4f88838aab
娄娈 <louluan@ofpay.com>
2018-11-30 11:38:40 +0800

    增加Eureka服务注册通知功能，具备广播能力，及时让服务集群内的机器感知

--- apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/service/eureka/enhancement/EurekaServerStateListener.java
+++ apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/service/eureka/enhancement/EurekaServerStateListener.java
@@ -8,6 +8,7 @@ import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.netflix.eureka.server.event.*;
 import org.springframework.context.event.EventListener;
 import org.springframework.http.HttpEntity;
@@ -17,10 +18,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Date;
+
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 
 @Service
 public class EurekaServerStateListener {
+
+    @Value("${notification.dingtalk.atMobiles}")
+    private String atMobiles;
+
+    @Value("${notification.dingtalk.token}")
+    private String token;
+
+
     private Logger logger = LoggerFactory.getLogger(EurekaServerStateListener.class);
     @Autowired
     private RestTemplate restTemplate;
@@ -32,24 +43,27 @@ public class EurekaServerStateListener {
 
     @EventListener
     public void listen(EurekaInstanceCanceledEvent eurekaInstanceCanceledEvent) {
-
-        updateStatus(eurekaInstanceCanceledEvent.getAppName(),eurekaInstanceCanceledEvent.getServerId(), InstanceInfo.InstanceStatus.DOWN);
-        //sendMessage("【提示】服务下线通知："+JSON.toJSONString(eurekaInstanceCanceledEvent));
+        if(!eurekaInstanceCanceledEvent.isReplication()){
+            updateStatus(eurekaInstanceCanceledEvent.getAppName(),eurekaInstanceCanceledEvent.getServerId(), InstanceInfo.InstanceStatus.DOWN);
+            sendMessage("【提示】服务下线通知：服务名称 "+eurekaInstanceCanceledEvent.getAppName()+ " 实例 " + eurekaInstanceCanceledEvent.getServerId()+" , 时间：" + new Date() );
+        }else{
+            logger.info("当前事件：{} 属于其他节点同步，不需要广播通知",eurekaInstanceCanceledEvent);
+        }
     }
 
-//    public void sendMessage(String message){
-//        try{
-//            JSONObject jsonObject = new JSONObject();
-//            jsonObject.put("msgtype","text");
-//            JSONObject text = new JSONObject();
-//            text.put("content",message);
-//            jsonObject.put("text",text);
-//            jsonObject.put("atMobiles","13270729135");
-//            restTemplate.postForEntity("https://oapi.dingtalk.com/robot/send?access_token=34fd7f65e64e356221cae3570fc68e066262b697cfe7b75372218da8a30f91b9",jsonObject,String.class);
-//        }catch (Exception e){
-//            //NOOP
-//        }
-//    }
+    public void sendMessage(String message){
+        try{
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("msgtype","text");
+            JSONObject text = new JSONObject();
+            text.put("content",message);
+            jsonObject.put("text",text);
+            jsonObject.put("atMobiles",atMobiles);
+            restTemplate.postForEntity("https://oapi.dingtalk.com/robot/send?access_token="+token,jsonObject,String.class);
+        }catch (Exception e){
+            //NOOP
+        }
+    }
 
 
     public void updateStatus(String appName, String instanceId, InstanceInfo.InstanceStatus status) {
@@ -64,8 +78,11 @@ public class EurekaServerStateListener {
         if (status.compareTo(InstanceInfo.InstanceStatus.DOWN) == 0) {
             logger.info("服务：{}，{}已经断线！", appName, instanceId);
         }
+        //更新实例的状态和最后更新时间戳
         InstanceInfo eventualInstanceInfo = eventInstanceInfo;
+        eventualInstanceInfo.setLastUpdatedTimestamp();
         eventualInstanceInfo.setStatus(status);
+
         Applications applications = instanceRegistry.getApplications();
         applications.getRegisteredApplications().forEach(application ->
                 application.getInstances().forEach(instanceInfo -> {
@@ -88,8 +105,13 @@ public class EurekaServerStateListener {
 
     @EventListener
     public void listen(EurekaInstanceRegisteredEvent event) {
-        logger.info("服务发生注册，开始通知其他服务");
-        updateStatus(event.getInstanceInfo().getAppName(),event.getInstanceInfo().getInstanceId(),event.getInstanceInfo().getStatus());
+        if(!event.isReplication()){
+            logger.info("服务发生注册，开始通知其他服务");
+            updateStatus(event.getInstanceInfo().getAppName(),event.getInstanceInfo().getInstanceId(),event.getInstanceInfo().getStatus());
+            sendMessage("【提示】服务上线通知：服务名称 "+event.getInstanceInfo().getAppName()+ " 实例 " + event.getInstanceInfo().getInstanceId()+" , 时间：" + new Date() );
+        }else{
+            logger.info("当前事件：{} 属于其他节点同步，不需要广播通知",event);
+        }
     }
 
     @EventListener
